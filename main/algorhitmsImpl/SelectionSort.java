25e264d980348e15b1e422265e519cfb01d29539
Volodymyr Kulesha <v.s.kulesha@gmail.com>
2023-07-11 21:13:53 +0300

    Move to new package

--- main/algorhitmsImpl/SelectionSort.java
+++ main/algorhitmsImpl/SelectionSort.java
@@ -1,5 +1,11 @@
-public class SelectionSort {
-    public static void sort(int[] array){
+package main.algorhitmsImpl;
+
+import main.SortAlgorithm;
+
+public class SelectionSort implements SortAlgorithm {
+
+    //https://www.geeksforgeeks.org/selection-sort/
+    public void sort(int[] array){
         for (int i = 0; i < array.length - 1; i++){
             int minValue = array[i];
             for (int j = i + 1; j < array.length;j++){
